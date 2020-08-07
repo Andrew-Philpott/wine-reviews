5b0c02ae011e8e28a9c9067bccd29aa4fae2c48b
Andrew Philpott <andrewphilpott92@gmail.com>
2020-08-07 13:18:07 -0700

    Updates

---
+++ precache-manifest.e58ced2508fed6ca9092fb90e32bb23c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392f0a87892a8cd4edf392f246da0363",
+    "url": "/wine-reviews/index.html"
+  },
+  {
+    "revision": "a63121ab87632a863f10",
+    "url": "/wine-reviews/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aeeed971bbb87d74f6f9",
+    "url": "/wine-reviews/static/js/2.d1c3b180.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wine-reviews/static/js/2.d1c3b180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63121ab87632a863f10",
+    "url": "/wine-reviews/static/js/main.c651cf58.chunk.js"
+  },
+  {
+    "revision": "b693ff263a909a6ec72b",
+    "url": "/wine-reviews/static/js/runtime-main.b8b03613.js"
+  }
+]);
