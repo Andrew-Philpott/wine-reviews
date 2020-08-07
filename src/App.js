0d56903137cb3e583228cfe12f3f0ecdcde527ef
Andrew Philpott <andrewphilpott92@gmail.com>
2020-08-07 10:46:23 -0700

    Create function to check local storage for reviews and set application state.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,45 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+function getReviewsFromLocalStorage() {
+  let reviews = localStorage.getItem("reviews");
+  let parsedReviews = null;
+  if (reviews) {
+    parsedReviews = JSON.parse(reviews);
+    return parsedReviews;
+  } else {
+    return null;
+  }
+}
+
 function App() {
   const [data, setData] = React.useState([]);
+  const [error, setError] = React.useState("");
 
   React.useEffect(() => {
     if (data.length === 0) {
-      (async () => {
-        const response = await fetch(
-          "https://lightninglaw.azurewebsites.net/api/reviews",
-          {
-            method: "GET",
+      const reviews = getReviewsFromLocalStorage();
+      if (reviews === null) {
+        (async () => {
+          try {
+            const response = await fetch(
+              "https://lightninglaw.azurewebsites.net/api/reviews",
+              {
+                method: "GET",
+              }
+            );
+            const data = await response.json();
+            localStorage.setItem("reviews", JSON.stringify(data));
+            setData(data);
+          } catch {
+            setError(
+              "We're sorry. Something went wrong on our end. Please try again later."
+            );
           }
-        );
-        const data = await response.json();
-        setData(data);
-      })();
+        })();
+      } else {
+        setData(reviews);
+      }
     }
   }, []);
 
