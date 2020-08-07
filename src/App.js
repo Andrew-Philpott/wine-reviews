367b23159a7e2f43dd83bc1d16dafd4a8bcaf60b
Andrew Philpott <andrewphilpott92@gmail.com>
2020-08-07 11:56:54 -0700

    Solve localstorage problem by creating smaller key:value pairs.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Grid, TextField, MenuItem } from "@material-ui/core";
+import { array } from "yargs";
 
 function getReviewsFromLocalStorage() {
   let reviews = localStorage.getItem("reviews");
@@ -14,7 +15,10 @@ function getReviewsFromLocalStorage() {
 }
 
 function setReviewsToLocalStorage(data) {
-  localStorage.setItem("reviews", JSON.stringify(data));
+  for (let i = 0; i < data.length; i++) {
+    const stringify = JSON.stringify(data[i]);
+    localStorage.setItem(`reviews${i}`, stringify);
+  }
 }
 
 function App() {
@@ -44,6 +48,7 @@ function App() {
             );
             const data = await response.json();
             setData(data);
+            setCountries([...new Set(data.map((x) => x.country))]);
             setReviewsToLocalStorage(data);
           } catch {
             setError(
@@ -56,7 +61,8 @@ function App() {
       }
     }
   }, []);
-
+  console.log(countries);
+  console.log(localStorage.getItem(""));
   return (
     <div className="App">
       <Grid container>
@@ -68,16 +74,54 @@ function App() {
         </Grid>
         <Grid item xs={6}>
           <h1>Countires of Origin</h1>
-          <TextField
-            select
-            name="countries"
-            value={inputs.country}
-            onChange={handleChange}
-            variant="outlined"
-            fullWidth
-          >
-            <MenuItem key={1} value={""}></MenuItem>
-          </TextField>
+          {countries ? (
+            <TextField
+              select
+              name="countries"
+              value={inputs.country}
+              onChange={handleChange}
+              variant="outlined"
+              fullWidth
+            >
+              {countries.values((x) => {
+                console.log(x);
+                return (
+                  <MenuItem key={1} value={x}>
+                    {x}
+                  </MenuItem>
+                );
+              })}
+            </TextField>
+          ) : (
+            <TextField
+              select
+              name="countries"
+              value={inputs.country}
+              onChange={handleChange}
+              variant="outlined"
+              fullWidth
+            >
+              <MenuItem key={1} value="">
+                {""}
+              </MenuItem>
+            </TextField>
+          )}
+        </Grid>
+        <Grid container>
+          <Grid item xs={6}>
+            Filters:{" "}
+            <TextField
+              type="text"
+              name="search"
+              value={inputs.search}
+              onChange={handleChange}
+              variant="outlined"
+            ></TextField>
+          </Grid>
+          <Grid item xs={6}>
+            <div style={{ float: "left" }}></div>
+            <div style={{ float: "right" }}></div>
+          </Grid>
         </Grid>
       </Grid>
     </div>
