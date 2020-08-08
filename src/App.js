32fd72dcbd58f88e82081aa0513fc5dafe1b2600
Andrew Philpott <andrewphilpott92@gmail.com>
2020-08-07 17:14:51 -0700

    Work on styling and changing search results.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import {
   Grid,
@@ -10,6 +10,7 @@ import {
   TableRow,
   TableCell,
   TableHead,
+  Container,
   TableBody,
 } from "@material-ui/core";
 import setReviewsToLs from "./setReviewsToLS";
@@ -22,31 +23,35 @@ function App() {
     country: "",
     search: "",
     results: 10,
-    queryString: "",
+    search: "",
   });
   const [results, setResults] = React.useState([]);
   const [error, setError] = React.useState("");
-  let currentResults = usePagination(data, inputs.results);
 
   function handleChange(e) {
     const { name, value } = e.target;
     setInputs((inputs) => ({ ...inputs, [name]: value }));
   }
 
-  function searchReviews(queryString = inputs.queryString) {
-    let newState = [...data];
-    let filteredReviews = [];
+  const searchReviews = (queryString) => {
+    if (queryString === "") {
+      setResults([...data]);
+    } else {
+      let newState = [...data];
+      let filteredReviews = [];
 
-    while (filteredReviews.length === 0) {
-      filteredReviews = newState.filter((x) => x.variety === queryString);
-      filteredReviews = newState.filter((x) => x.title === queryString);
-      filteredReviews = newState.filter((x) => x.winery === queryString);
-      filteredReviews = newState.filter((x) => x.tasterName === queryString);
-      break;
+      while (filteredReviews.length === 0) {
+        filteredReviews = newState.filter((x) => x.variety === queryString);
+        filteredReviews = newState.filter((x) => x.title === queryString);
+        filteredReviews = newState.filter((x) => x.winery === queryString);
+        filteredReviews = newState.filter((x) => x.tasterName === queryString);
+        break;
+      }
+      setResults(filteredReviews);
     }
-    setResults(filteredReviews);
+
     return;
-  }
+  };
 
   React.useEffect(() => {
     if (data.length === 0) {
@@ -61,6 +66,7 @@ function App() {
             );
             const data = await response.json();
             setData(data);
+            setResults(data);
             setReviewsToLs(data);
           } catch {
             setError(
@@ -73,135 +79,179 @@ function App() {
       }
     }
   }, [data]);
-
+  let currentResults = usePagination(data, inputs.results);
   console.log(data);
   return (
     <div className="App">
       <Grid container>
-        <Grid item xs={6}>
-          <h1>TOTAL NUMBER OF REVIEWS</h1>
-          <Grid style={{ height: "500px", border: "2px solid black" }}>
-            <h1>number</h1>
-          </Grid>
-        </Grid>
-        <Grid item xs={6}>
-          <h1>Countries of Origin</h1>
-          {data ? (
-            <TextField
-              select
-              name="country"
-              value={inputs.country}
-              onChange={handleChange}
-              variant="outlined"
-              fullWidth
-            >
-              {Object.values([...new Set(data.map((x) => x.country))]).map(
-                (x, index) => {
-                  return (
-                    <MenuItem key={index} value={x}>
-                      {x}
-                    </MenuItem>
-                  );
-                }
-              )}
-            </TextField>
-          ) : (
-            <TextField
-              select
-              name="countries"
-              value={inputs.country}
-              onChange={handleChange}
-              variant="outlined"
-              fullWidth
-            >
-              <MenuItem key={1} value="">
-                {""}
-              </MenuItem>
-            </TextField>
-          )}
-        </Grid>
-        <Grid container>
-          <Grid item xs={6}>
-            Filters:{" "}
-            <TextField
-              type="text"
-              name="search"
-              placeholder="Search term"
-              value={inputs.search}
-              onChange={handleChange}
-              variant="outlined"
-            ></TextField>
-          </Grid>
-          <Grid item xs={6}>
-            <Grid container>
-              <Grid item xs={4}>
-                <Button onClick={() => searchReviews()}>SEARCH</Button>
+        <Grid item xs={1} />
+        <Grid item xs={10}>
+          <Grid container>
+            <Grid item xs={1} />
+            <Grid item xs={4}>
+              <h1>TOTAL NUMBER OF REVIEWS</h1>
+              <Grid
+                container
+                alignItems="center"
+                justify="center"
+                style={{ height: "200px", border: "2px solid black" }}
+              >
+                <span style={{ fontSize: "12vw" }}>
+                  {data && inputs.country === ""
+                    ? ""
+                    : data &&
+                      data.filter((x) => x.country === inputs.country).length}
+                </span>
               </Grid>
-              <Grid item xs={8}>
-                <TextField
-                  style={{ float: "right" }}
-                  select
-                  name="results"
-                  value={inputs.results}
-                  onChange={handleChange}
-                  variant="outlined"
-                  fullWidth
-                >
-                  <MenuItem key={10} value={10}>
-                    10
-                  </MenuItem>
-                  <MenuItem key={20} value={20}>
-                    20
-                  </MenuItem>
-                  <MenuItem key={30} value={30}>
-                    30
-                  </MenuItem>
-                </TextField>
+              <Grid item xs={1} />
+            </Grid>
+            <Grid item xs={6}>
+              <Grid container>
+                <Grid item xs={3} />
+                <Grid item xs={8}>
+                  <h1>Countries of Origin</h1>
+                  {data ? (
+                    <TextField
+                      select
+                      name="country"
+                      value={inputs.country}
+                      onChange={handleChange}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      {Object.values([
+                        ...new Set(data.map((x) => x.country)),
+                      ]).map((x, index) => {
+                        return (
+                          <MenuItem key={index} value={x}>
+                            {x}
+                          </MenuItem>
+                        );
+                      })}
+                    </TextField>
+                  ) : (
+                    <TextField
+                      select
+                      name="countries"
+                      value={inputs.country}
+                      onChange={handleChange}
+                      variant="outlined"
+                      fullWidth
+                    >
+                      <MenuItem key={1} value="">
+                        {""}
+                      </MenuItem>
+                    </TextField>
+                  )}
+                </Grid>
+                <Grid item xs={1} />
               </Grid>
+            </Grid>
+            <Grid style={{ marginTop: "80px" }} container>
+              <Grid item xs={1} />
+              <Grid item xs={10}>
+                <Grid container>
+                  <Grid item xs={9}>
+                    <Grid alignItems="center" container>
+                      <span style={{ marginRight: "10px" }}>Filters:</span>
+                      <TextField
+                        type="text"
+                        name="search"
+                        style={{ width: "60%", marginRight: "10px" }}
+                        placeholder="Search term"
+                        value={inputs.search}
+                        onChange={handleChange}
+                        variant="outlined"
+                      />
+
+                      <Button
+                        variant="outlined"
+                        onClick={() => searchReviews(inputs.search)}
+                      >
+                        SEARCH
+                      </Button>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <div
+                      style={{
+                        float: "right",
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      <span style={{ marginRight: "5px" }}>Results:</span>
+                      <TextField
+                        select
+                        name="results"
+                        value={inputs.results}
+                        onChange={handleChange}
+                        variant="outlined"
+                      >
+                        <MenuItem key={10} value={10}>
+                          10
+                        </MenuItem>
+                        <MenuItem key={20} value={20}>
+                          20
+                        </MenuItem>
+                        <MenuItem key={30} value={30}>
+                          30
+                        </MenuItem>
+                      </TextField>
+                    </div>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={1} />
+            </Grid>
+            <Grid item xs={12}>
+              <TableContainer>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Title</TableCell>
+                      <TableCell>Variety</TableCell>
+                      <TableCell>Winery</TableCell>
+                      <TableCell>Points</TableCell>
+                      <TableCell>Price</TableCell>
+                      <TableCell>Taster</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {data.length !== 0 &&
+                      currentResults.currentData().map((result, index) => {
+                        if (result !== null) {
+                          return (
+                            <TableRow key={index}>
+                              <TableCell>
+                                {result.title && result.title}
+                              </TableCell>
+                              <TableCell>
+                                {result.variety && result.variety}
+                              </TableCell>
+                              <TableCell>
+                                {result.winery && result.winery}
+                              </TableCell>
+                              <TableCell>
+                                {result.points && result.points}
+                              </TableCell>
+                              <TableCell>
+                                {result.price && result.price}
+                              </TableCell>
+                              <TableCell>
+                                {result.tasterName && result.tasterName}
+                              </TableCell>
+                            </TableRow>
+                          );
+                        }
+                      })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Title</TableCell>
-                  <TableCell>Variety</TableCell>
-                  <TableCell>Winery</TableCell>
-                  <TableCell>Points</TableCell>
-                  <TableCell>Price</TableCell>
-                  <TableCell>Taster</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.length !== 0 &&
-                  currentResults.currentData().map((result, index) => {
-                    if (result !== null) {
-                      return (
-                        <TableRow key={index}>
-                          <TableCell>{result.title && result.title}</TableCell>
-                          <TableCell>
-                            {result.variety && result.variety}
-                          </TableCell>
-                          <TableCell>
-                            {result.winery && result.winery}
-                          </TableCell>
-                          <TableCell>
-                            {result.points && result.points}
-                          </TableCell>
-                          <TableCell>{result.price && result.price}</TableCell>
-                          <TableCell>
-                            {result.tasterName && result.tasterName}
-                          </TableCell>
-                        </TableRow>
-                      );
-                    }
-                  })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
+        <Grid item xs={1} />
       </Grid>
     </div>
   );
